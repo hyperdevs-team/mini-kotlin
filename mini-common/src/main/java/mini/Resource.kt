d9e5f343121ff0e761e4998be39c4f89527ebd06
Adrián García <adrian.garcia@bq.com>
2019-11-14 15:19:44 +0100

    Add equals and hashCode to Resource

--- mini-common/src/main/java/mini/Resource.kt
+++ mini-common/src/main/java/mini/Resource.kt
@@ -56,6 +56,19 @@ open class Resource<out T> @PublishedApi internal constructor(val value: Any?) {
     override fun toString(): String {
         return value.toString()
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Resource<*>
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = value?.hashCode() ?: 0
 }
 
 /**
