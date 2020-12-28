f9776789bca7d7ef31086f06b49867b1b0175f01
Estefanía Sarasola Elvira <estefania.sarasola@gmail.com>
2020-12-28 08:35:03 +0100

    Update mini-kodein-android/src/main/java/mini/kodein/android/KodeinAndroidUtils.kt
    Co-authored-by: Adrián García <adriangarcia.lopez@gmail.com>

--- mini-kodein-android/src/main/java/mini/kodein/android/KodeinAndroidUtils.kt
+++ mini-kodein-android/src/main/java/mini/kodein/android/KodeinAndroidUtils.kt
@@ -85,15 +85,12 @@ inline fun <reified T : Any, reified VM : TypedViewModel<T>, A> A.viewModel(para
  * Requires previous [ViewModelProvider.Factory] injection for the [ViewModel] via [bindViewModelFactory]
  * to work and a [TypedViewModel] to be used.
  */
-// FIXME: This is failing after the migration to Kodein 7.1.0. It seems not being properly aware that
-//        T is reified, so [params] doesn't need to be a [Typed<TypedValue(T)>] and that it can be
-//        directly of type T
-//@MainThread
-//inline fun <reified T, reified VM : TypedViewModel<T>, F> F.viewModel(params: T): Lazy<VM> where F : DIAware, F : Fragment {
-//    return lazy {
-//        ViewModelProvider(this, direct.instance(VM::class.java, params)).get(VM::class.java)
-//    }
-//}
+@MainThread
+inline fun <reified T : Any, reified VM : TypedViewModel<T>, F> F.viewModel(params: T): Lazy<VM> where F : DIAware, F : Fragment {
+    return lazy {
+        ViewModelProvider(this, direct.instance(VM::class.java, params)).get(VM::class.java)
+    }
+}
 
 /**
  * Injects a [ViewModel] with an [Activity] context that implements [DIAware
